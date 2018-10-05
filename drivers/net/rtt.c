7542f05c8af8dab6fa7a5572ff6380bcdfa60aab
Dominik Kilian <Dominik.Kilian@nordicsemi.no>
2018-10-05 15:21:39 +0200

    Reformat code accordind to zephyr coding style

--- drivers/net/rtt.c
+++ drivers/net/rtt.c
@@ -74,28 +74,26 @@ struct eth_rtt_context {
 };
 
 static const u8_t reset_frame_data[] = {
-		0, 0, 0, 0, 0, 0,            // dummy destination MAC address
-		0, 0, 0, 0, 0, 0,            // dummy source MAC address
-		254, 255,                    // custom eth type
-		216, 33, 105, 148, 78, 111,  // randomly generated magic payload
-		203, 53, 32, 137, 247, 122,  // randomly generated magic payload
-		100, 72, 129, 255, 204, 173, // randomly generated magic payload
-		};
+	0, 0, 0, 0, 0, 0,            // dummy destination MAC address
+	0, 0, 0, 0, 0, 0,            // dummy source MAC address
+	254, 255,                    // custom eth type
+	216, 33, 105, 148, 78, 111,  // randomly generated magic payload
+	203, 53, 32, 137, 247, 122,  // randomly generated magic payload
+	100, 72, 129, 255, 204, 173, // randomly generated magic payload
+	};
 
 static struct eth_rtt_context context_data;
 
-#if (SYS_LOG_LEVEL >= SYS_LOG_LEVEL_DEBUG) && defined(CONFIG_ETH_RTT_DEBUG_HEX_DUMP)
+#if (SYS_LOG_LEVEL >= SYS_LOG_LEVEL_DEBUG) \
+	&& defined(CONFIG_ETH_RTT_DEBUG_HEX_DUMP)
 
-void DBG_HEX_DUMP(const char* prefix, const u8_t* data, int length)
-{
+void DBG_HEX_DUMP(const char* prefix, const u8_t* data, int length) {
 	int i;
 	char line[64];
 	char *ptr;
-	while (length > 0)
-	{
+	while (length > 0) {
 		ptr = line;
-		for (i = 0; i < 16 && length > 0; i++)
-		{
+		for (i = 0; i < 16 && length > 0; i++) {
 			ptr += sprintf(ptr, " %02X", *data);
 			data++;
 			length--;
@@ -126,43 +124,44 @@ static void rtt_send_begin(struct eth_rtt_context *context)
 	context->crc = 0xFFFF;
 }
 
-static void rtt_send_fragment(struct eth_rtt_context *context, const u8_t *ptr, int len)
+static void rtt_send_fragment(struct eth_rtt_context *context, const u8_t *ptr,
+	int len)
 {
+	static const u8_t end_stuffed[2] = { SLIP_ESC, SLIP_ESC_END };
+	static const u8_t esc_stuffed[2] = { SLIP_ESC, SLIP_ESC_ESC };
 	const u8_t *end = ptr + len;
 	const u8_t *plain_begin = ptr;
 
 	context->crc = crc16_ccitt(context->crc, ptr, len);
 
-	while (ptr < end)
-	{
-		if (*ptr == SLIP_END || *ptr == SLIP_ESC)
-		{
-			if (ptr > plain_begin)
-			{
-				SEGGER_RTT_Write(CONFIG_ETH_RTT_CHANNEL, plain_begin, ptr - plain_begin);
-				DBG_HEX_DUMP("RTT<", plain_begin, ptr - plain_begin);
+	while (ptr < end) {
+		if (*ptr == SLIP_END || *ptr == SLIP_ESC) {
+			if (ptr > plain_begin) {
+				SEGGER_RTT_Write(CONFIG_ETH_RTT_CHANNEL,
+					plain_begin, ptr - plain_begin);
+				DBG_HEX_DUMP("RTT<", plain_begin,
+					ptr - plain_begin);
 			}
 
-			if (*ptr == SLIP_END)
-			{
-				static const u8_t data[2] = { SLIP_ESC, SLIP_ESC_END };
-				SEGGER_RTT_Write(CONFIG_ETH_RTT_CHANNEL, data, sizeof(data));
-				DBG_HEX_DUMP("RTT<", data, sizeof(data));
-			}
-			else if (*ptr == SLIP_ESC)
-			{
-				static const u8_t data[2] = { SLIP_ESC, SLIP_ESC_ESC };
-				SEGGER_RTT_Write(CONFIG_ETH_RTT_CHANNEL, data, sizeof(data));
-				DBG_HEX_DUMP("RTT<", data, sizeof(data));
+			if (*ptr == SLIP_END) {
+				SEGGER_RTT_Write(CONFIG_ETH_RTT_CHANNEL,
+					end_stuffed, sizeof(end_stuffed));
+				DBG_HEX_DUMP("RTT<", end_stuffed,
+					sizeof(end_stuffed));
+			} else if (*ptr == SLIP_ESC) {
+				SEGGER_RTT_Write(CONFIG_ETH_RTT_CHANNEL,
+					esc_stuffed, sizeof(esc_stuffed));
+				DBG_HEX_DUMP("RTT<", esc_stuffed,
+					sizeof(esc_stuffed));
 			}
 			plain_begin = ptr + 1;
 		}
 		ptr++;
 	}
 
-	if (ptr > plain_begin)
-	{
-		SEGGER_RTT_Write(CONFIG_ETH_RTT_CHANNEL, plain_begin, ptr - plain_begin);
+	if (ptr > plain_begin) {
+		SEGGER_RTT_Write(CONFIG_ETH_RTT_CHANNEL, plain_begin,
+			ptr - plain_begin);
 		DBG_HEX_DUMP("RTT<", plain_begin, ptr - plain_begin);
 	}
 }
@@ -202,8 +201,7 @@ static int eth_iface_send(struct net_if *iface, struct net_pkt *pkt)
 	DBG_HEX_DUMP("ETH>", net_pkt_ll(pkt), net_pkt_ll_reserve(pkt));
 	rtt_send_fragment(context, net_pkt_ll(pkt), net_pkt_ll_reserve(pkt));
 
-	for (frag = pkt->frags; frag; frag = frag->frags)
-	{
+	for (frag = pkt->frags; frag; frag = frag->frags) {
 		DBG_HEX_DUMP("ETH>", frag->data, frag->len);
 		rtt_send_fragment(context, frag->data, frag->len);
 	};
@@ -212,7 +210,7 @@ static int eth_iface_send(struct net_if *iface, struct net_pkt *pkt)
 	rtt_send_end(context);
 
 	net_pkt_unref(pkt);
-	
+
 	return 0;
 }
 
@@ -224,18 +222,15 @@ static void recv_frame(struct eth_rtt_context *context, u8_t* data, int len)
 	struct net_buf *pkt_buf = NULL;
 	struct net_buf *last_buf = NULL;
 
-	if (len <= 2)
-	{
-		if (len > 0)
-		{
+	if (len <= 2) {
+		if (len > 0) {
 			SYS_LOG_ERR("Invalid frame length");
 		}
 		return;
 	}
 
 	u16_t crc16 = crc16_ccitt(0xFFFF, data, len - 2);
-	if (data[len - 2] != (crc16 >> 8) || data[len - 1] != (crc16 & 0xFF))
-	{
+	if (data[len - 2] != (crc16 >> 8) || data[len - 1] != (crc16 & 0xFF)) {
 		SYS_LOG_ERR("Invalid frame CRC");
 		return;
 	}
@@ -252,8 +247,7 @@ static void recv_frame(struct eth_rtt_context *context, u8_t* data, int len)
 
 	DBG_HEX_DUMP_BEGIN("ETH<");
 
-	while (len > 0)
-	{
+	while (len > 0) {
 		pkt_buf = net_pkt_get_frag(pkt, K_NO_WAIT);
 		if (!pkt_buf) {
 			SYS_LOG_ERR("Could not allocate data for rx pkt");
@@ -290,32 +284,26 @@ static void recv_frame(struct eth_rtt_context *context, u8_t* data, int len)
 	net_recv_data(context->iface, pkt);
 }
 
-static void decode_new_slip_data(struct eth_rtt_context *context, int new_data_size)
+static void decode_new_slip_data(struct eth_rtt_context *context,
+	int new_data_size)
 {
 	u8_t *src = &context->rx_buffer[context->rx_buffer_length];
 	u8_t *dst = &context->rx_buffer[context->rx_buffer_length];
 	u8_t *end = src + new_data_size;
-	u8_t *frame_start = context->rx_buffer;
+	u8_t *start = context->rx_buffer;
 	u8_t last_byte = context->rx_buffer_length > 0 ? dst[-1] : 0;
 
-	while (src < end)
-	{
+	while (src < end) {
 		u8_t byte = *src++;
 		*dst++ = byte;
-		if (byte == SLIP_END)
-		{
-			recv_frame(context, frame_start, dst - frame_start - 1);
-			frame_start = dst;
-		}
-		else if (last_byte == SLIP_ESC)
-		{
-			if (byte == SLIP_ESC_END)
-			{
+		if (byte == SLIP_END) {
+			recv_frame(context, start, dst - start - 1);
+			start = dst;
+		} else if (last_byte == SLIP_ESC) {
+			if (byte == SLIP_ESC_END) {
 				dst--;
 				dst[-1] = SLIP_END;
-			}
-			else if (byte == SLIP_ESC_ESC)
-			{
+			} else if (byte == SLIP_ESC_ESC) {
 				dst--;
 				dst[-1] = SLIP_ESC;
 			}
@@ -323,11 +311,10 @@ static void decode_new_slip_data(struct eth_rtt_context *context, int new_data_s
 		last_byte = byte;
 	}
 
-	context->rx_buffer_length = dst - frame_start;
+	context->rx_buffer_length = dst - start;
 
-	if (context->rx_buffer_length > 0 && frame_start != context->rx_buffer)
-	{
-		memmove(context->rx_buffer, frame_start, context->rx_buffer_length);
+	if (context->rx_buffer_length > 0 && start != context->rx_buffer) {
+		memmove(context->rx_buffer, start, context->rx_buffer_length);
 	}
 }
 
@@ -342,17 +329,18 @@ static void poll_work_handler(struct k_work *work)
 	int num;
 
 	do {
-		if (context->rx_buffer_length >= sizeof(context->rx_buffer))
-		{
-			SYS_LOG_ERR("RX buffer overflow. Discarding buffer contents.\n");
+		if (context->rx_buffer_length >= sizeof(context->rx_buffer)) {
+			SYS_LOG_ERR("RX buffer overflow. "
+				"Discarding buffer contents.\n");
 			context->rx_buffer_length = 0;
 		}
 		num = SEGGER_RTT_Read(CONFIG_ETH_RTT_CHANNEL,
+			&context->rx_buffer[context->rx_buffer_length],
+			sizeof(context->rx_buffer) - context->rx_buffer_length);
+		if (num > 0) {
+			DBG_HEX_DUMP("RTT>",
 				&context->rx_buffer[context->rx_buffer_length],
-				sizeof(context->rx_buffer) - context->rx_buffer_length);
-		if (num > 0)
-		{
-			DBG_HEX_DUMP("RTT>", &context->rx_buffer[context->rx_buffer_length], num);
+				num);
 			decode_new_slip_data(context, num);
 			period = K_MSEC(CONFIG_ETH_POLL_ACTIVE_PERIOD_MS);
 		}
@@ -365,7 +353,7 @@ K_WORK_DEFINE(eth_rtt_poll_work, poll_work_handler);
 
 static void poll_timer_handler(struct k_timer *dummy)
 {
-    k_work_submit(&eth_rtt_poll_work);
+	k_work_submit(&eth_rtt_poll_work);
 }
 
 /******** COMMON PART OF THE DRIVER (initialization on configuration) ********/
@@ -388,12 +376,12 @@ static void eth_iface_init(struct net_if *iface)
 #if defined(CONFIG_ETH_RTT_MAC_ADDR)
 	if (CONFIG_ETH_RTT_MAC_ADDR[0] != 0) {
 		mac_addr_configured = (net_bytes_from_str(context->mac_addr,
-				sizeof(context->mac_addr), CONFIG_ETH_RTT_MAC_ADDR) >= 0);
+			sizeof(context->mac_addr),
+			CONFIG_ETH_RTT_MAC_ADDR) >= 0);
 	}
 #endif
 
-	if (!mac_addr_configured)
-	{
+	if (!mac_addr_configured) {
 		context->mac_addr[0] = 0x00;
 		context->mac_addr[1] = 0x00;
 		context->mac_addr[2] = 0x5E;
@@ -402,23 +390,20 @@ static void eth_iface_init(struct net_if *iface)
 		context->mac_addr[5] = sys_rand32_get();
 	}
 
-	net_if_set_link_addr(iface, context->mac_addr, sizeof(context->mac_addr),
-			     NET_LINK_ETHERNET);
+	net_if_set_link_addr(iface, context->mac_addr,
+		sizeof(context->mac_addr), NET_LINK_ETHERNET);
 
-	k_timer_start(&eth_rtt_poll_timer, K_MSEC(CONFIG_ETH_POLL_PERIOD_MS), K_MSEC(5000));
+	k_timer_start(&eth_rtt_poll_timer, K_MSEC(CONFIG_ETH_POLL_PERIOD_MS),
+		K_MSEC(5000));
 
-	SYS_LOG_INF("Initialized '%s': MAC addr %02X:%02X:%02X:%02X:%02X:%02X, MTU %d, RTT channel %d, RAM consumed %d",
-		iface->if_dev->dev->config->name,
-		context->mac_addr[0],
-		context->mac_addr[1],
-		context->mac_addr[2],
-		context->mac_addr[3],
-		context->mac_addr[4],
-		context->mac_addr[5],
-		CONFIG_ETH_RTT_MTU,
-		CONFIG_ETH_RTT_CHANNEL,
-		sizeof(*context)
-		);
+	SYS_LOG_INF(
+		"Initialized '%s': MAC addr %02X:%02X:%02X:%02X:%02X:%02X, "
+		"MTU %d, RTT channel %d, RAM consumed %d",
+		iface->if_dev->dev->config->name, context->mac_addr[0],
+		context->mac_addr[1], context->mac_addr[2],
+		context->mac_addr[3], context->mac_addr[4],
+		context->mac_addr[5], CONFIG_ETH_RTT_MTU,
+		CONFIG_ETH_RTT_CHANNEL, sizeof(*context));
 
 	rtt_send_begin(context);
 	rtt_send_fragment(context, reset_frame_data, sizeof(reset_frame_data));
@@ -435,18 +420,22 @@ static int eth_rtt_init(struct device *dev)
 {
 	struct eth_rtt_context *context = dev->driver_data;
 
-	SEGGER_RTT_ConfigUpBuffer(CONFIG_ETH_RTT_CHANNEL, CHANNEL_NAME, context->rtt_up_buffer, sizeof(context->rtt_up_buffer), SEGGER_RTT_MODE_BLOCK_IF_FIFO_FULL);
-	SEGGER_RTT_ConfigDownBuffer(CONFIG_ETH_RTT_CHANNEL, CHANNEL_NAME, context->rtt_down_buffer, sizeof(context->rtt_down_buffer), SEGGER_RTT_MODE_BLOCK_IF_FIFO_FULL);
+	SEGGER_RTT_ConfigUpBuffer(CONFIG_ETH_RTT_CHANNEL, CHANNEL_NAME,
+		context->rtt_up_buffer, sizeof(context->rtt_up_buffer),
+		SEGGER_RTT_MODE_BLOCK_IF_FIFO_FULL);
+	SEGGER_RTT_ConfigDownBuffer(CONFIG_ETH_RTT_CHANNEL, CHANNEL_NAME,
+		context->rtt_down_buffer,
+		sizeof(context->rtt_down_buffer),
+		SEGGER_RTT_MODE_BLOCK_IF_FIFO_FULL);
 
 	return 0;
 }
 
 static const struct ethernet_api if_api = {
 	.iface_api.init = eth_iface_init,
-	.iface_api.send = eth_iface_send,
-	.get_capabilities = eth_capabilities,
-};
+	.iface_api.send = eth_iface_send, .get_capabilities =
+		eth_capabilities, };
 
 ETH_NET_DEVICE_INIT(eth_rtt, CONFIG_ETH_RTT_DRV_NAME, eth_rtt_init,
-		&context_data, NULL, CONFIG_KERNEL_INIT_PRIORITY_DEFAULT, &if_api,
-		CONFIG_ETH_RTT_MTU);
+	&context_data, NULL, CONFIG_KERNEL_INIT_PRIORITY_DEFAULT,
+	&if_api, CONFIG_ETH_RTT_MTU);
