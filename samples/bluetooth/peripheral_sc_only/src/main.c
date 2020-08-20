80e3d0e8598e0b8bc5b41fb8190017b5229a675d
Kamil Piszczek <Kamil.Piszczek@nordicsemi.no>
2020-08-20 08:16:43 -0400

    BT_RPC: sc_only samples: extend connection api usage

--- samples/bluetooth/peripheral_sc_only/src/main.c
+++ samples/bluetooth/peripheral_sc_only/src/main.c
@@ -24,22 +24,48 @@ static const struct bt_data ad[] = {
 	BT_DATA_BYTES(BT_DATA_FLAGS, (BT_LE_AD_GENERAL | BT_LE_AD_NO_BREDR)),
 };
 
-static void connected(struct bt_conn *conn, uint8_t err)
+static void connected(struct bt_conn *conn, uint8_t conn_err)
 {
+	int err;
 	char addr[BT_ADDR_LE_STR_LEN];
+	struct bt_conn_le_phy_param phy_params;
 
 	bt_addr_le_to_str(bt_conn_get_dst(conn), addr, sizeof(addr));
 
-	if (err) {
-		printk("Failed to connect to %s (%u)\n", addr, err);
+	if (conn_err) {
+		printk("Failed to connect to %s (%u)\n", addr, conn_err);
 		return;
 	}
 
 	printk("Connected %s\n", addr);
 
-	if (bt_conn_set_security(conn, BT_SECURITY_L4)) {
-		printk("Failed to set security\n");
+	err = bt_conn_set_security(conn, BT_SECURITY_L4);
+	if (err) {
+		printk("Failed to set security: %d\n", err);
 	}
+
+	err = bt_conn_le_param_update(conn,
+		BT_LE_CONN_PARAM(20, 50, 0, 500));
+	if (err) {
+		printk("Failed to update conn params: %d\n", err);
+	}
+
+	err = bt_conn_le_data_len_update(conn,
+		BT_LE_DATA_LEN_PARAM_MAX);
+	if (err) {
+		printk("Failed to update DLE: %d\n", err);
+	}
+
+	memset(&phy_params, 0, sizeof(phy_params));
+	phy_params.options = BT_CONN_LE_PHY_OPT_CODED_S8;
+	phy_params.pref_tx_phy = BT_GAP_LE_PHY_CODED;
+	phy_params.pref_rx_phy = BT_GAP_LE_PHY_CODED;
+
+	err = bt_conn_le_phy_update(conn,
+		&phy_params);
+	if (err) {
+		printk("Failed to update PHY: %d\n", err);
+	}	
 }
 
 static void disconnected(struct bt_conn *conn, uint8_t reason)
@@ -78,11 +104,51 @@ static void security_changed(struct bt_conn *conn, bt_security_t level,
 	}
 }
 
+static bool le_param_req(struct bt_conn *conn, struct bt_le_conn_param *param)
+{
+	printk("Accepting connection parameters request\n");
+
+	return true;
+}
+
+static void le_param_updated(struct bt_conn *conn, uint16_t interval,
+			     uint16_t latency, uint16_t timeout)
+{
+	printk("Updating connection parameters: interval %d, latency: %d, "
+		"timeout %d\n", interval, latency, timeout);
+}
+
+static void remote_info_available(struct bt_conn *conn,
+				  struct bt_conn_remote_info *remote_info)
+{
+	printk("Remote LL version: %d.%d\n", remote_info->version,
+		remote_info->subversion);
+}
+
+static void le_phy_updated(struct bt_conn *conn,
+			   struct bt_conn_le_phy_info *param)
+{
+	printk("TX PHY: %d\n", param->tx_phy);
+	printk("RX PHY: %d\n", param->rx_phy);
+}
+
+static 	void le_data_len_updated(struct bt_conn *conn,
+				 struct bt_conn_le_data_len_info *info)
+{
+	printk("TX len: %d\n", info->tx_max_len);
+	printk("RX len: %d\n", info->rx_max_len);
+}
+
 static struct bt_conn_cb conn_callbacks = {
 	.connected = connected,
 	.disconnected = disconnected,
 	.identity_resolved = identity_resolved,
 	.security_changed = security_changed,
+	.le_param_req = le_param_req,
+	.le_param_updated = le_param_updated,
+	.remote_info_available = remote_info_available,
+	.le_phy_updated = le_phy_updated,
+	.le_data_len_updated = le_data_len_updated,
 };
 
 static void auth_passkey_display(struct bt_conn *conn, unsigned int passkey)
